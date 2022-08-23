a7949a19e85c24d6afda31c45d550332f0035280
Richard Lasco <lasco9@gmail.com>
2022-08-23 15:45:43 -0500

    Changed App from functional to class component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import React from 'react';
 import './App.css';
-import CurriculumVitae from './components/CurriculumVitae';
+// import CurriculumVitae from './components/CurriculumVitae';
 import Form from './components/Form';
 
-function App() {
-  return (
-    <div className='App'>
-      <section className='form-side'>
-        <Form />
-      </section>
-      {/* <section className='cv-side'>
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <section className='form-side'>
+          <Form />
+        </section>
+        {/* <section className='cv-side'>
         <CurriculumVitae />
       </section> */}
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
