a2d2295c5988d2fb04bd554af3b2af9b099c753f
Richard Lasco <lasco9@gmail.com>
2022-09-02 15:57:56 -0500

    Compartmentalized CV

---
+++ src/components/CVEducationSection.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../styles/CVEducationSection.css';
+
+class CVEducationSection extends React.Component {
+  render() {
+    return (
+      <section className='cv-education-info'>
+        <h2 className='cv-education-title'>Education</h2>
+        <section className='cv-education-container'>
+          <h3 className='cv-education-name'>{this.props.educationInfo.name}</h3>
+          <p className='cv-education-city'>{this.props.educationInfo.city}</p>
+          <p className='cv-education-degree'>
+            {this.props.educationInfo.degree}
+          </p>
+          <p className='cv-education-date'>
+            {this.props.educationInfo.startDate} to{' '}
+            {this.props.educationInfo.endDate}
+          </p>
+          <p className='cv-education-description'>
+            {this.props.educationInfo.description}
+          </p>
+        </section>
+      </section>
+    );
+  }
+}
+
+export default CVEducationSection;
---
+++ src/components/CVProfessionalSection.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../styles/CVProfessionalSection.css';
+
+class CVProfessionalSection extends React.Component {
+  render() {
+    return (
+      <section className='cv-professional-info'>
+        <h2 className='cv-professional-title'>Professional History</h2>
+        <section className='cv-professional-container'>
+          <h3 className='cv-professional-name'>
+            {this.props.professionalInfo.name}
+          </h3>
+          <h4 className='cv-professional-role'>
+            {this.props.professionalInfo.role}
+          </h4>
+          <p className='cv-professional-date'>
+            {this.props.professionalInfo.startDate} to{' '}
+            {this.props.professionalInfo.endDate}
+          </p>
+          <p className='cv-professional-description'>
+            {this.props.professionalInfo.description}
+          </p>
+        </section>
+      </section>
+    );
+  }
+}
+
+export default CVProfessionalSection;
