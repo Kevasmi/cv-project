89a5cd7ee763e61fc0af7a8414d19bae1cf4af14
Richard Lasco <lasco9@gmail.com>
2022-09-02 22:15:59 -0500

    Added edit button to component

--- src/components/CurriculumVitae.js
+++ src/components/CurriculumVitae.js
@@ -8,6 +8,9 @@ class CurriculumVitae extends React.Component {
   render() {
     return (
       <div className='cv-container'>
+        <button className='edit-button'>
+          <span class='material-symbols-outlined'>edit</span>
+        </button>
         <CVPersonalSection personalInfo={this.props.personalInfo} />
         <CVProfessionalSection professionalInfo={this.props.professionalInfo} />
         <CVEducationSection educationInfo={this.props.educationInfo} />
