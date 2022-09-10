13d5844a778dc25d58f8e660803a9d2812328335
Richard Lasco <lasco9@gmail.com>
2022-09-09 19:21:22 -0500

    Created CVProfessionalList component

---
+++ src/components/CVProfessionalList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CVProfessionalSection from './CVProfessionalSection';
+
+class CVProfessionalList extends React.Component {
+  render() {
+    console.log(this.props.professionalInfo);
+    const itemArray = this.props.professionalInfo;
+    const professionalItems = itemArray.map((professionalItem, i) => (
+      <CVProfessionalSection
+        key={professionalItem.id}
+        professionalItem={professionalItem}
+      />
+    ));
+
+    return <ul>{professionalItems}</ul>;
+  }
+}
+
+export default CVProfessionalList;
