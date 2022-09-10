2aebd8f007f5f887276cc5244343edb75554841e
Richard Lasco <lasco9@gmail.com>
2022-09-09 19:23:02 -0500

    Adjusted CVProfessionalSection values

--- src/components/CVProfessionalSection.js
+++ src/components/CVProfessionalSection.js
@@ -5,23 +5,19 @@ class CVProfessionalSection extends React.Component {
   render() {
     return (
       <section className='cv-professional-info'>
-        <h2 className='cv-professional-title'>Professional History</h2>
         <section className='cv-professional-container'>
-          {/* <button className='edit-button-professional'>
-            <span class='material-symbols-outlined'>edit</span>
-          </button> */}
           <h3 className='cv-professional-name'>
-            {this.props.professionalInfo.name}
+            {this.props.professionalItem.name}
           </h3>
           <h4 className='cv-professional-role'>
-            {this.props.professionalInfo.role}
+            {this.props.professionalItem.role}
           </h4>
           <p className='cv-professional-date'>
-            {this.props.professionalInfo.startDate} to{' '}
-            {this.props.professionalInfo.endDate}
+            {this.props.professionalItem.startDate} to{' '}
+            {this.props.professionalItem.endDate}
           </p>
           <p className='cv-professional-description'>
-            {this.props.professionalInfo.description}
+            {this.props.professionalItem.description}
           </p>
         </section>
       </section>
