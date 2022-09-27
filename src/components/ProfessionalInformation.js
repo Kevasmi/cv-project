1819a15b6ae944551bb11d91882057e2af188284
Richard Lasco <lasco9@gmail.com>
2022-09-27 16:45:52 -0500

    Refactored ProfessionalInfo to functional component

--- src/components/ProfessionalInformation.js
+++ src/components/ProfessionalInformation.js
@@ -1,83 +1,71 @@
 import React from 'react';
 
-class ProfessionalInformation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleProfessionalChange = this.handleProfessionalChange.bind(this);
-    this.handleRemove = this.handleRemove.bind(this);
-  }
+const ProfessionalInformation = (props) => {
+  const handleProfessionalChange = (e) => {
+    props.onChange(e, props.id);
+  };
 
-  handleProfessionalChange(e) {
-    this.props.onChange(e, this.props.id);
-  }
+  const handleRemove = () => {
+    props.onRemove(props.id);
+  };
 
-  handleRemove() {
-    this.props.onRemove(this.props.id);
-  }
-
-  render() {
-    return (
-      <section className='company-info'>
-        <button
-          type='button'
-          className='delete-button'
-          onClick={this.handleRemove}
-        >
-          X
-        </button>
-        <label htmlFor='company-name'>Company Name </label>
+  return (
+    <section className='company-info'>
+      <button type='button' className='delete-button' onClick={handleRemove}>
+        X
+      </button>
+      <label htmlFor='company-name'>Company Name </label>
+      <input
+        type='text'
+        id='company-name'
+        name='name'
+        value={props.professionalItem.name}
+        onChange={handleProfessionalChange}
+        required
+      ></input>
+      <label htmlFor='role'>Role </label>
+      <input
+        type='text'
+        id='role'
+        name='role'
+        value={props.professionalItem.role}
+        onChange={handleProfessionalChange}
+        required
+      ></input>
+      <section className='dates'>
+        <label htmlFor='start-date' className='start-label'>
+          Start date{' '}
+        </label>
         <input
-          type='text'
-          id='company-name'
-          name='name'
-          value={this.props.professionalItem.name}
-          onChange={this.handleProfessionalChange}
+          type='date'
+          id='start-date'
+          name='startDate'
+          value={props.professionalItem.startDate}
+          onChange={handleProfessionalChange}
           required
         ></input>
-        <label htmlFor='role'>Role </label>
+        <label htmlFor='end-date' className='end-label'>
+          End date{' '}
+        </label>
         <input
-          type='text'
-          id='role'
-          name='role'
-          value={this.props.professionalItem.role}
-          onChange={this.handleProfessionalChange}
+          type='date'
+          id='end-date'
+          name='endDate'
+          value={props.professionalItem.endDate}
+          onChange={handleProfessionalChange}
           required
         ></input>
-        <section className='dates'>
-          <label htmlFor='start-date' className='start-label'>
-            Start date{' '}
-          </label>
-          <input
-            type='date'
-            id='start-date'
-            name='startDate'
-            value={this.props.professionalItem.startDate}
-            onChange={this.handleProfessionalChange}
-            required
-          ></input>
-          <label htmlFor='end-date' className='end-label'>
-            End date{' '}
-          </label>
-          <input
-            type='date'
-            id='end-date'
-            name='endDate'
-            value={this.props.professionalItem.endDate}
-            onChange={this.handleProfessionalChange}
-            required
-          ></input>
-        </section>
-        <label htmlFor='description'>Description of duties </label>
-        <textarea
-          id='description'
-          name='description'
-          value={this.props.professionalItem.description}
-          onChange={this.handleProfessionalChange}
-          required
-        ></textarea>
       </section>
-    );
-  }
-}
+      <label htmlFor='description'>Description of duties </label>
+      <textarea
+        id='description'
+        name='description'
+        value={props.professionalItem.description}
+        onChange={handleProfessionalChange}
+        required
+      ></textarea>
+    </section>
+  );
+};
 
 export default ProfessionalInformation;
