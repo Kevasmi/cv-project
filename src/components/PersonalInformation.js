ad9eedfe9a79a980f62886e4740ab7b2ec8934a8
Richard Lasco <lasco9@gmail.com>
2022-09-27 16:44:06 -0500

    Refactored PersonalInfo to functional component

--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -1,67 +1,60 @@
 import React from 'react';
 
-class PersonalInformation extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handlePersonalInputChange = this.handlePersonalInputChange.bind(this);
-  }
+const PersonalInformation = (props) => {
+  const handlePersonalInputChange = (e) => {
+    props.onPersonalInputChange(e);
+  };
 
-  handlePersonalInputChange(e) {
-    this.props.onPersonalInputChange(e);
-  }
-
-  render() {
-    return (
-      <section className='personal-section'>
-        <label htmlFor='name'>Name </label>
+  return (
+    <section className='personal-section'>
+      <label htmlFor='name'>Name </label>
+      <input
+        type='text'
+        id='name'
+        placeholder='e.g. Richard Lasco'
+        name='name'
+        value={props.personalInfo.name}
+        onChange={handlePersonalInputChange}
+        required
+      ></input>
+      <section className='phone-and-email'>
+        <label htmlFor='phone' className='phone-label'>
+          Phone{' '}
+        </label>
         <input
-          type='text'
-          id='name'
-          placeholder='e.g. Richard Lasco'
-          name='name'
-          value={this.props.personalInfo.name}
-          onChange={this.handlePersonalInputChange}
+          type='tel'
+          id='phone'
+          placeholder='e.g. 012-345-6789'
+          name='phone'
+          value={props.personalInfo.phone}
+          onChange={handlePersonalInputChange}
           required
         ></input>
-        <section className='phone-and-email'>
-          <label htmlFor='phone' className='phone-label'>
-            Phone{' '}
-          </label>
-          <input
-            type='tel'
-            id='phone'
-            placeholder='e.g. 012-345-6789'
-            name='phone'
-            value={this.props.personalInfo.phone}
-            onChange={this.handlePersonalInputChange}
-            required
-          ></input>
-          <label htmlFor='email' className='email-label'>
-            E-Mail{' '}
-          </label>
-          <input
-            type='email'
-            id='email'
-            placeholder='e.g. lasco@gmail.com'
-            name='email'
-            value={this.props.personalInfo.email}
-            onChange={this.handlePersonalInputChange}
-            required
-          ></input>
-        </section>
-        <label htmlFor='home-address'>Address </label>
+        <label htmlFor='email' className='email-label'>
+          E-Mail{' '}
+        </label>
         <input
-          type='text'
-          id='home-address'
-          placeholder='e.g. 114 West Copper Sage Circle, TX'
-          name='address'
-          value={this.props.personalInfo.address}
-          onChange={this.handlePersonalInputChange}
+          type='email'
+          id='email'
+          placeholder='e.g. lasco@gmail.com'
+          name='email'
+          value={props.personalInfo.email}
+          onChange={handlePersonalInputChange}
           required
         ></input>
       </section>
-    );
-  }
-}
+      <label htmlFor='home-address'>Address </label>
+      <input
+        type='text'
+        id='home-address'
+        placeholder='e.g. 114 West Copper Sage Circle, TX'
+        name='address'
+        value={props.personalInfo.address}
+        onChange={handlePersonalInputChange}
+        required
+      ></input>
+    </section>
+  );
+};
 
 export default PersonalInformation;
