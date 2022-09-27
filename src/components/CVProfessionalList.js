18f74b343c122985202c7b800293a0f7c0b057ed
Richard Lasco <lasco9@gmail.com>
2022-09-27 16:56:40 -0500

    Refactored CVProfessionalList to functional component

--- src/components/CVProfessionalList.js
+++ src/components/CVProfessionalList.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import CVProfessionalSection from './CVProfessionalSection';
 
-class CVProfessionalList extends React.Component {
-  render() {
-    console.log(this.props.professionalInfo);
-    const itemArray = this.props.professionalInfo;
-    const professionalItems = itemArray.map((professionalItem, i) => (
-      <CVProfessionalSection
-        key={professionalItem.id}
-        professionalItem={professionalItem}
-      />
-    ));
+const CVProfessionalList = (props) => {
+  const itemArray = props.professionalInfo;
+  const professionalItems = itemArray.map((professionalItem, i) => (
+    <CVProfessionalSection
+      key={professionalItem.id}
+      professionalItem={professionalItem}
+    />
+  ));
 
-    return (
-      <ul className='cv-professional-list'>
-        {professionalItems.length ? professionalItems : ''}
-      </ul>
-    );
-  }
-}
+  return (
+    <ul className='cv-professional-list'>
+      {professionalItems.length ? professionalItems : ''}
+    </ul>
+  );
+};
 
 export default CVProfessionalList;
