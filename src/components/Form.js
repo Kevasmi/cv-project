947b10f9c215daf4c49768e7dbe99d00a506b9fd
Richard Lasco <lasco9@gmail.com>
2022-09-27 16:18:19 -0500

    Refactored Form.js to be a functional component

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,45 +4,39 @@ import EducationList from './EducationList';
 import PersonalInformation from './PersonalInformation';
 import ProfessionalList from './ProfessionalList';
 
-class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const Form = (props) => {
+  const handleSubmit = (e) => {
+    props.onSubmitEvent(e);
+  };
 
-  handleSubmit(e) {
-    this.props.onSubmitEvent(e);
-  }
+  let content = (
+    <div className='form-container'>
+      <form onSubmit={handleSubmit}>
+        <h2 className='form-personal-header'>Personal Information</h2>
+        <PersonalInformation
+          personalInfo={props.info.personalInfo}
+          onPersonalInputChange={props.onPersonalInputChange}
+        />
+        <ProfessionalList
+          professionalInfo={props.info.professionalInfo}
+          onAdd={props.onAddProfessionalSection}
+          onChange={props.onProfessionalChange}
+          onRemove={props.onRemoveProfessionalSection}
+        />
+        <EducationList
+          educationInfo={props.info.educationInfo}
+          onChange={props.onEducationChange}
+          onAdd={props.onAddEducationSection}
+          onRemove={props.onRemoveEducationSection}
+        />
 
-  render() {
-    return (
-      <div className='form-container'>
-        <form onSubmit={this.handleSubmit}>
-          <h2 className='form-personal-header'>Personal Information</h2>
-          <PersonalInformation
-            personalInfo={this.props.info.personalInfo}
-            onPersonalInputChange={this.props.onPersonalInputChange}
-          />
-          <ProfessionalList
-            professionalInfo={this.props.info.professionalInfo}
-            onAdd={this.props.onAddProfessionalSection}
-            onChange={this.props.onProfessionalChange}
-            onRemove={this.props.onRemoveProfessionalSection}
-          />
-          <EducationList
-            educationInfo={this.props.info.educationInfo}
-            onChange={this.props.onEducationChange}
-            onAdd={this.props.onAddEducationSection}
-            onRemove={this.props.onRemoveEducationSection}
-          />
-
-          <button type='submit' className='submitBtn'>
-            Submit
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+        <button type='submit' className='submitBtn'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+  return content;
+};
 
 export default Form;
