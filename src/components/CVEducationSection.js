07aae795e32a224e29b1297fb55514e3a66c56d6
Richard Lasco <lasco9@gmail.com>
2022-09-27 16:57:57 -0500

    Refactored CVEducationSection to functional component

--- src/components/CVEducationSection.js
+++ src/components/CVEducationSection.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import '../styles/CVEducationSection.css';
 
-class CVEducationSection extends React.Component {
-  render() {
-    return (
-      <section className='cv-education-info'>
-        <section className='cv-education-container'>
-          <h3 className='cv-education-name'>{this.props.educationItem.name}</h3>
-          <p className='cv-education-city'>{this.props.educationItem.city}</p>
-          <p className='cv-education-degree'>
-            {this.props.educationItem.degree}
-          </p>
-          <p className='cv-education-date'>
-            {this.props.educationItem.startDate} to{' '}
-            {this.props.educationItem.endDate}
-          </p>
-          <p className='cv-education-description'>
-            {this.props.educationItem.description}
-          </p>
-        </section>
+const CVEducationSection = (props) => {
+  return (
+    <section className='cv-education-info'>
+      <section className='cv-education-container'>
+        <h3 className='cv-education-name'>{props.educationItem.name}</h3>
+        <p className='cv-education-city'>{props.educationItem.city}</p>
+        <p className='cv-education-degree'>{props.educationItem.degree}</p>
+        <p className='cv-education-date'>
+          {props.educationItem.startDate} to {props.educationItem.endDate}
+        </p>
+        <p className='cv-education-description'>
+          {props.educationItem.description}
+        </p>
       </section>
-    );
-  }
-}
+    </section>
+  );
+};
 
 export default CVEducationSection;
