dd8144dab83742e75c717b7bf9fa4f9cdab58b8c
Richard Lasco <lasco9@gmail.com>
2022-09-27 16:57:29 -0500

    Refactored CVProfessionalSection to funcl component

--- src/components/CVProfessionalSection.js
+++ src/components/CVProfessionalSection.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import '../styles/CVProfessionalSection.css';
 
-class CVProfessionalSection extends React.Component {
-  render() {
-    return (
-      <section className='cv-professional-info'>
-        <section className='cv-professional-container'>
-          <h3 className='cv-professional-name'>
-            {this.props.professionalItem.name}
-          </h3>
-          <h4 className='cv-professional-role'>
-            {this.props.professionalItem.role}
-          </h4>
-          <p className='cv-professional-date'>
-            {this.props.professionalItem.startDate} to{' '}
-            {this.props.professionalItem.endDate}
-          </p>
-          <p className='cv-professional-description'>
-            {this.props.professionalItem.description}
-          </p>
-        </section>
+const CVProfessionalSection = (props) => {
+  return (
+    <section className='cv-professional-info'>
+      <section className='cv-professional-container'>
+        <h3 className='cv-professional-name'>{props.professionalItem.name}</h3>
+        <h4 className='cv-professional-role'>{props.professionalItem.role}</h4>
+        <p className='cv-professional-date'>
+          {props.professionalItem.startDate} to {props.professionalItem.endDate}
+        </p>
+        <p className='cv-professional-description'>
+          {props.professionalItem.description}
+        </p>
       </section>
-    );
-  }
-}
+    </section>
+  );
+};
 
 export default CVProfessionalSection;
