de7417293c23218696718ccd56ea125629902391
Richard Lasco <lasco9@gmail.com>
2022-09-27 16:45:08 -0500

    Refactored EducationList to functional component

--- src/components/EducationList.js
+++ src/components/EducationList.js
@@ -1,49 +1,42 @@
 import React from 'react';
 import EducationInformation from './EducationInformation';
 
-class EducationList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAdd = this.handleAdd.bind(this);
-  }
+const EducationList = (props) => {
+  const handleAdd = () => {
+    props.onAdd();
+  };
 
-  handleAdd() {
-    this.props.onAdd();
-  }
-
-  render() {
-    const itemArray = this.props.educationInfo;
-    const educationItems = itemArray.map((educationItem) => (
-      <EducationInformation
-        key={educationItem.id}
-        id={educationItem.id}
-        educationItem={educationItem}
-        onChange={this.props.onChange}
-        onRemove={this.props.onRemove}
-      />
-    ));
-    return (
-      <div className='education-list'>
-        <h2 className='form-education-header'>
-          Education Information{' '}
-          <span>
-            <button
-              type='button'
-              className='education-header-button'
-              onClick={this.handleAdd}
-            >
-              + Add
-            </button>
-          </span>
-        </h2>
-        {educationItems.length ? (
-          <div>{educationItems}</div>
-        ) : (
-          <p>No sections added.</p>
-        )}
-      </div>
-    );
-  }
-}
+  const itemArray = props.educationInfo;
+  const educationItems = itemArray.map((educationItem) => (
+    <EducationInformation
+      key={educationItem.id}
+      id={educationItem.id}
+      educationItem={educationItem}
+      onChange={props.onChange}
+      onRemove={props.onRemove}
+    />
+  ));
+  return (
+    <div className='education-list'>
+      <h2 className='form-education-header'>
+        Education Information{' '}
+        <span>
+          <button
+            type='button'
+            className='education-header-button'
+            onClick={handleAdd}
+          >
+            + Add
+          </button>
+        </span>
+      </h2>
+      {educationItems.length ? (
+        <div>{educationItems}</div>
+      ) : (
+        <p>No sections added.</p>
+      )}
+    </div>
+  );
+};
 
 export default EducationList;
