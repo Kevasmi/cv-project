b1b8767a6b462fb8e02504b2d22302ea630198f6
Richard Lasco <lasco9@gmail.com>
2022-09-27 16:45:34 -0500

    Refactored ProfessionalList to functional component

--- src/components/ProfessionalList.js
+++ src/components/ProfessionalList.js
@@ -1,51 +1,43 @@
 import React from 'react';
 import ProfessionalInformation from './ProfessionalInformation';
 
-class ProfessionalList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAddProfessionalSection =
-      this.handleAddProfessionalSection.bind(this);
-  }
+const ProfessionalList = (props) => {
+  const handleAddProfessionalSection = () => {
+    props.onAdd();
+  };
 
-  handleAddProfessionalSection() {
-    this.props.onAdd();
-  }
+  const itemArray = props.professionalInfo;
+  const professionalItems = itemArray.map((professionalItem) => (
+    <ProfessionalInformation
+      key={professionalItem.id}
+      id={professionalItem.id}
+      professionalItem={professionalItem}
+      onChange={props.onChange}
+      onRemove={props.onRemove}
+    />
+  ));
 
-  render() {
-    const itemArray = this.props.professionalInfo;
-    const professionalItems = itemArray.map((professionalItem) => (
-      <ProfessionalInformation
-        key={professionalItem.id}
-        id={professionalItem.id}
-        professionalItem={professionalItem}
-        onChange={this.props.onChange}
-        onRemove={this.props.onRemove}
-      />
-    ));
-
-    return (
-      <div className='professional-list'>
-        <h2 className='form-professional-header'>
-          Professional Information{' '}
-          <span>
-            <button
-              type='button'
-              className='professional-header-button'
-              onClick={this.handleAddProfessionalSection}
-            >
-              + Add
-            </button>
-          </span>
-        </h2>
-        {professionalItems.length ? (
-          <div>{professionalItems}</div>
-        ) : (
-          <p>No sections added.</p>
-        )}
-      </div>
-    );
-  }
-}
+  return (
+    <div className='professional-list'>
+      <h2 className='form-professional-header'>
+        Professional Information{' '}
+        <span>
+          <button
+            type='button'
+            className='professional-header-button'
+            onClick={handleAddProfessionalSection}
+          >
+            + Add
+          </button>
+        </span>
+      </h2>
+      {professionalItems.length ? (
+        <div>{professionalItems}</div>
+      ) : (
+        <p>No sections added.</p>
+      )}
+    </div>
+  );
+};
 
 export default ProfessionalList;
