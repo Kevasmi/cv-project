acb33caa84984ff2fce7afa8e3e18fca663d5e98
Richard Lasco <lasco9@gmail.com>
2022-09-27 16:55:31 -0500

    Refactored CVPersonalSection to functional component

--- src/components/CVPersonalSection.js
+++ src/components/CVPersonalSection.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import '../styles/CVPersonalSection.css';
 
-class CVPersonalSection extends React.Component {
-  render() {
-    return (
-      <section className='cv-personal-info'>
-        <h1 className='cv-name'>{this.props.personalInfo.name}</h1>
-        <header className='cv-header'>
-          <ul>
-            <li className='cv-phone-number'>
-              <span>{this.props.personalInfo.phone}</span>
-            </li>
-            <li className='cv-email'>
-              <span>{this.props.personalInfo.email}</span>
-            </li>
-            <li className='cv-address'>
-              <span>{this.props.personalInfo.address}</span>
-            </li>
-            <li></li>
-          </ul>
-        </header>
-      </section>
-    );
-  }
-}
+const CVPersonalSection = (props) => {
+  return (
+    <section className='cv-personal-info'>
+      <h1 className='cv-name'>{props.personalInfo.name}</h1>
+      <header className='cv-header'>
+        <ul>
+          <li className='cv-phone-number'>
+            <span>{props.personalInfo.phone}</span>
+          </li>
+          <li className='cv-email'>
+            <span>{props.personalInfo.email}</span>
+          </li>
+          <li className='cv-address'>
+            <span>{props.personalInfo.address}</span>
+          </li>
+          <li></li>
+        </ul>
+      </header>
+    </section>
+  );
+};
 
 export default CVPersonalSection;
